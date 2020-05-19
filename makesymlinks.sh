7487e3da054ef4489c2fad9887cbf4303b5f3ab9
Simon Schmidt <pomminger@gmail.com>
2020-05-19 09:26:37 +0200

    update makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,9 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+files="bashrc p10k.zsh tmux.conf vimrc zshrc"    
+# list of files/folders to symlink in homedir
+
 
 ##########
 
@@ -33,10 +35,6 @@ done
 install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/robbyrussell/oh-my-zsh.git
-    fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
         chsh -s $(which zsh)
@@ -45,21 +43,16 @@ else
     # If zsh isn't installed, get the platform of the current machine
     platform=$(uname);
     # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
-            install_zsh
-        fi
-        if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
-            install_zsh
-        fi
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
+    if [[ -f /etc/redhat-release ]]; then
+        sudo yum install zsh
+        install_zsh
+    fi
+    if [[ -f /etc/debian_version ]]; then
+        sudo apt-get install zsh
+        install_zsh
     fi
 fi
 }
 
 install_zsh
+
